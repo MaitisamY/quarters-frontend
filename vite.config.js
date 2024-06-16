71be4213fb7be0a88ba9a9b13f9f813d567962cf
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-16 09:00:25 +0500

    3rd push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config(); // Load environment variables from .env file
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.NODE_ENV === 'production' ? 8080 : (process.env.VITE_PORT || 5173),
+    // other server options...
+  }
+});
