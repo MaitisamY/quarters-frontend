197acbd5c3e5fc2f3877596817c9084fc061f35d
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-16 11:46:49 +0500

    10th push

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,10 @@ dotenv.config(); // Load environment variables from .env file
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: process.env.NODE_ENV === 'production' ? 8080 : (process.env.VITE_PORT || 5173),
-  //   // other server options...
-  // },
-  // build: {
-  //   outDir: 'build', // Specify the output directory for the build
-  //   assetsDir: '.', // Assets directory relative to the output directory
-  //   sourcemap: false, // Disable source maps for production
-  //   // other build options...
-  // }
+  build: {
+    outDir: 'build', // Specify the output directory for the build
+    assetsDir: '.', // Assets directory relative to the output directory
+    sourcemap: false, // Disable source maps for production
+    // other build options...
+  }
 });
